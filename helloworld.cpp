8d9ef97f03d6aad345c59c743c823b2fb9b9578a
RockFor <igor90bond@gmail.com>
2014-09-20 10:34:03 +0500

    third commit

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main() {
-	for(int i = 0; i < 10; i++)
-	cout << "Hello world!";
+	for(int i = 0; i < 10; i++) 	// Создаем цикл
+	cout << "Hello world!";		// Выводим сообщение
 	return 0;
 }
